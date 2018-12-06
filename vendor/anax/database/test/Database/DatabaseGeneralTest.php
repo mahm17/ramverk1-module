b80984474d04c62935a633ac321ba4f6d351ff21
mahm17 <macki.najs@hotmail.com>
2018-12-06 10:16:13 +0000

    Commited all files

---
+++ vendor/anax/database/test/Database/DatabaseGeneralTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Anax\Database;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+* Test the database class, general tests.
+*/
+class DatabaseGeneralTest extends TestCase
+{
+    /**
+     * Testcase
+     */
+    public function testCreateDatabaseObject()
+    {
+        $this->assertTrue(true);
+    }
+}
